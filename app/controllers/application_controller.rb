dd9fa189b666be2bfbbf9df93b4532a0fdeb4621
da-yoshi-k <hey234234s@gmail.com>
2022-12-11 00:48:44 +0900

    add: cloud vision APIの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  require "google/cloud/vision/v1"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :documents do
     get 'texts', to: 'documents#execute_ocr'
+    get 'vision_texts', to: 'documents#execute_vision_api'
   end
   root to: 'documents#index'
 end
